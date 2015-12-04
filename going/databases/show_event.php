6975e4b187738702d82aa5578b23432c7389de04
francisquinha <francisquinha@gmail.com>
2015-12-04 09:21:14 +0000

    bug ao visualizar evento sem estar logado

--- going/databases/show_event.php
+++ going/databases/show_event.php
@@ -17,43 +17,46 @@ include_once("templates/show_event.php");
 ?>
 
 <!-- Comment form -->
-<?php if ($logged && (isRegisteredInEvent($id, $_SESSION['username'])) || ($_SESSION['username'] == $event['creator']) ) {?>
-    <form action="action_comment.php" method="post" >
-        <input type="hidden" name="id" value="<?=$id?>">
-        <input type="hidden" name="author" value="<?=$_SESSION['username']?>">
-        <input type="textarea" name="text">
-        <br>
-        <input type="submit" name="comment_btn" value="Comment">
-    </form>
-<?php } else { ?>
+<?php if ($logged) { ?>
+    <?php if (isRegisteredInEvent($id, $_SESSION['username']) || ($_SESSION['username'] == $event['creator'])) { ?>
+        <form action="action_comment.php" method="post">
+            <input type="hidden" name="id" value="<?= $id ?>">
+            <input type="hidden" name="author" value="<?= $_SESSION['username'] ?>">
+            <input type="textarea" name="text">
+            <br>
+            <input type="submit" name="comment_btn" value="Comment">
+        </form>
+    <?php } ?>
+<?php }
+else { ?>
     <p>Register in the event to comment</p>
 <?php } ?>
 
 <div class="nav_buttons">
         <?php if ($logged) { ?>
             <?php if (!isRegisteredInEvent($id, $_SESSION['username'])) {?>
-                
+
 			<button type="button" onclick="location.href='action_register_event.php?id=<?=$id?>'">Register</button>
-				
+
             <?php } else {?>
 			<button type="button" onclick="location.href='action_unregister_event.php?id=<?=$id?>'">Unregister</button>
-                
+
             <?php } ?>
-			
+
             <?php if ($_SESSION['username'] == $event['creator']
                                            || $_SESSION['username'] == 'admin') { ?>
 			<button type="button" onclick="location.href='action_delete_event.php?id=<?=$id?>'">Delete</button>
-                
+
             <?php } ?>
-			
+
 			 <?php if ($_SESSION['username'] == $event['creator']
                                            || $_SESSION['username'] == 'admin') { ?>
 			<button type="button" onclick="location.href='read_edit_event.php?id=<?=$id?>'">Edit</button>
-                
+
             <?php } ?>
-			
-			
-			
+
+
+
         <?php } ?>
 <button type="button" onclick="location.href='list_events.php'">Back</button>
 </div>
