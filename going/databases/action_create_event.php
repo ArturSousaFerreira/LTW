2009b92b2e5a5748a46c54ff9448bb1b51abe053
francisquinha <francisquinha@gmail.com>
2015-12-04 23:46:04 +0000

    tinha feito um erro na submissão anterior

--- going/databases/action_create_event.php
+++ going/databases/action_create_event.php
@@ -2,7 +2,7 @@
 include_once("events.php");
 
 if (isset($_POST['create_btn'])) {
-    createEvent($_POST['date'], $_POST['description'], $_POST['type'], $_POST['creator']);
+    createEvent($_POST['date'], $_POST['description'], $_POST['type'], $_POST['creator'], $_FILES['image']);
 }
 
 header('Location: ' . './list_events.php');
